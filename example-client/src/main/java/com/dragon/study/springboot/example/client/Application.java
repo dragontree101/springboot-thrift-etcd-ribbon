71453efc329713de95b5c9aee553d53f7f65ca2a
longzhe <dragonlong1986@126.com>
2016-07-07 15:33:31 +0800

    增加了client demo

---
+++ example-client/src/main/java/com/dragon/study/springboot/example/client/Application.java
@@ -0,0 +1,16 @@
+package com.dragon.study.springboot.example.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dragon on 16/6/18.
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication app = new SpringApplication(Application.class);
+    app.run(args);
+  }
+}
